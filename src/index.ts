0ec6dc612439b6e355539bbb9c46423234bf839f
uaqtpi <pep.g.sinjorgo@gmail.com>
2025-01-25 20:39:05 -0500

    v2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { serve } from "@hono/node-server";
+import { handle } from "hono/vercel";
 import { Hono } from "hono";
 // import { logger } from "hono/logger";
 import { Routes } from "./lib/apiSchema.js";
@@ -19,6 +20,15 @@ app.get("/", (c) => {
 	return c.json({ message: "OK" });
 });
 
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
+
+/*
 const port = 3001;
 
 serve({
@@ -30,3 +40,4 @@ console.log(process.env.MONGO_DB);
 console.log(
 	`[RONGO] Server is running on port ${port}\nOpen http://localhost:${port} to view it in the browser.`
 );
+*/
